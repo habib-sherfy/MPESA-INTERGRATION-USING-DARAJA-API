0dcaa9b6caa1c8d83cd5f91a6812ed68fdd4f522
habib_sherfy <shafynoor82@gmail.com>
2024-04-22 09:33:15 +0300

    added stkpush files

---
+++ stk_push.php
@@ -0,0 +1,27 @@
+
+<?
+// $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
+// curl_setopt($ch, CURLOPT_HTTPHEADER, [
+//     'Authorization: Bearer SUTdmeLgeRnehOk6GivoKqbj6G5M',
+//     'Content-Type: application/json'
+// ]);
+// curl_setopt($ch, CURLOPT_POST, 1);
+// curl_setopt($ch, CURLOPT_POSTFIELDS,
+//  {
+//     "BusinessShortCode": 174379,
+//     "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwNDIyMDkwNDQ4",
+//     "Timestamp": "20240422090448",
+//     "TransactionType": "CustomerPayBillOnline",
+//     "Amount": 1,
+//     "PartyA": 254708374149,
+//     "PartyB": 174379,
+//     "PhoneNumber": 254708374149,
+//     "CallBackURL": "https://mydomain.com/path",
+//     "AccountReference": "CompanyXLTD",
+//     "TransactionDesc": "Payment of X" 
+//   }
+// );
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// $response     = curl_exec($ch);
+// curl_close($ch);
+// echo $response;
