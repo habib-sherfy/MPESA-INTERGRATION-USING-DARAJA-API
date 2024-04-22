436b4bd13fca98bc75575698d855afcbc400d2d2
habib_sherfy <shafynoor82@gmail.com>
2024-04-22 11:43:05 +0300

    access token file works

--- access_token.php
+++ access_token.php
@@ -1,11 +1,27 @@
 <?php
 
-$consumer_key= 'MF2gqv72iqwfqRWQepM3RGyo18RDevSCNtCeIHVAfwflWVt2' ;
-$consumer_secret_key= '3X6udbqG1fMhMpIAUDsvsGJ7d795DwLbpkf8IOYqCFcAAcEjiDinjQoPhAXPhBgH' ;
+// For you to access any api you need to have an access token first.
+// So this is the first step to take and consider.
 
-$ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==']);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_exec($ch);
-curl_close($ch);
-echo $response;
+
+$consumer_key= 'MF2gqv72iqwfqRWQepM3RGyo18RDevSCNtCeIHVAfwflWVt2' ;
+$consumer_secret= '3X6udbqG1fMhMpIAUDsvsGJ7d795DwLbpkf8IOYqCFcAAcEjiDinjQoPhAXPhBgH' ;
+
+// This url is for mpesa endpoint 
+
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+// header for stk push
+$headers = ['Content-Type:application/json; charset=utf8'];
+
+
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumer_key . ':' . $consumer_secret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+echo $access_token = $result->access_token;
+curl_close($curl);
